73536546e54090c3b83f22a645b8ca128ddd2da8
pamelaolney <pjolney12@gmail.com>
2025-03-04 01:15:08 -0500

    Test submitting form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,18 +5,22 @@ describe('template spec', () => {
   it('visit portfolio homepage', () => {
     cy.url().should('include', 'index.html')
   })
-  it('click on contact form', () => {
+  it('fill out contact form', () => {
     cy.get('#contact-form-button')
       .invoke("removeAttr", "target")
       .click()
 
     cy.origin('https://form.jotform.com', () => {
+      const unique_user = ('test_name_' + Date.now())
+
       cy.get('[data-component="header"]').should('have.text', 'Contact Pamela')
+      cy.get('[name="q1_name1"]').type(unique_user)
+      cy.get('[name="q3_email"]').type(unique_user + "@email.com")
+      cy.get('[name="q4_message"]').type("Message from " + unique_user)
+      cy.get('.submit-button').click()
     })
-  })
-  it('submit contact form', () => {
-    //fill out form
-    //submit form
-    //expect form successful submission page
+    cy.origin('https://submit.jotform.com', () => {
+      cy.get('.thankyou-main-text').should('have.text', "Thank You!")
+    })
   })
 })
